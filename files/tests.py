6753ec45b3177c5dc7038e0294abd8a2bc4fac10
Edwar Carranza <edwarcarranza@gmail.com>
2021-10-28 12:32:15 +1100

    It was included unit tests to the file

--- files/tests.py
+++ files/tests.py
@@ -1,3 +1,49 @@
+import requests
+
+# Django
 from django.test import TestCase
 
-# Create your tests here.
+# Models
+from files.models import FileAllowed
+
+class FileSuccessTestCase(TestCase):
+    """ Files Allowed test case """
+    
+    def test_failed_file_allowed_creation(self):
+        """ Random codes should be generate automatically """
+        fileAllowed = FileAllowed.objects.create(
+            name= 'JSON',
+            extension= 'json',
+            is_active= True
+        )
+
+    def test_get_files_check_status_code_equals_200(self):
+        response = requests.get("http://localhost:8000/files/")
+        assert response.status_code == 200
+
+    def test_get_allowed_files_check_status_code_equals_200(self):
+        response = requests.get("http://localhost:8000/files/files_allowed")
+        assert response.status_code == 200
+
+class FileFailedTestCase(TestCase):
+    """ Files Allowed test case """
+    
+    def test_failed_file_allowed_creation(self):
+        """ Random codes should be generate automatically """
+        fileAllowed = FileAllowed.objects.create(
+            name= 'JSON',
+            extension= 'json',
+            is_active= True
+        )
+
+    def test_get_files_check_status_code_equals_200(self):
+        response = requests.get("http://localhost:8000/files/")
+        assert response.status_code == 200
+
+    def test_get_filtered_files_check_status_code_equals_200(self):
+        response = requests.get("http://localhost:8000/files/filtered_files/")
+        assert response.status_code == 200
+
+    def test_put_file_settings_check_status_code_equals_200(self):
+        response = requests.get("http://localhost:8000/files/file_settings/")
+        assert response.status_code == 200
